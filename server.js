9455b37e2950607442cb990508ab5192598072e8
DouglasGsilva <goDouglasdev@gmail.com>
2025-01-20 09:58:19 -0300

    nada demais

--- server.js
+++ server.js
@@ -1,9 +1,2 @@
-import app from "./src/app.js";
-
-const PORT = process.env.PORT || 3002;
-app.listen(PORT, () => {
-  console.log(`Servidor Rodando na Porta ${PORT}`);
-});
-
 // douglasDev
 // jgCl0ZRvPj2dP1ha
