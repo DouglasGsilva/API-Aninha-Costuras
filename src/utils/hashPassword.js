c8c392cb1bd83e5f3ad9c6a26a3fdd0d269c0d96
DouglasGsilva <goDouglasdev@gmail.com>
2025-01-20 09:56:04 -0300

    mudança de framework express

--- src/utils/hashPassword.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-const testPassword = async (password) => {
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-  return hashedPassword;
-};
-
-testPassword(88774401).then(console.log());
